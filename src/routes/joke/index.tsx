1bccd6d6aa52889c46d99703c7d616332ed04cad
Luis Yan Pederico <luisyanpederico@gmail.com>
2023-04-23 19:02:47 +0300

    create

---
+++ src/routes/joke/index.tsx
@@ -0,0 +1,25 @@
+import { component$ } from '@builder.io/qwik';
+import { routeLoader$ } from '@builder.io/qwik-city';
+ 
+export const useDadJoke = routeLoader$(async () => {
+    debugger;
+  const response = await fetch('https://icanhazdadjoke.com/', {
+    headers: { Accept: 'application/json' },
+  });
+  return (await response.json()) as {
+    id: string;
+    status: number;
+    joke: string;
+  };
+});
+ 
+export default component$(() => {
+  // Calling our `useDadJoke` hook, will return a reactive signal to the loaded data.
+  const dadJokeSignal = useDadJoke();
+  debugger;
+  return (
+    <section class="section bright">
+      <p>{dadJokeSignal.value.joke}</p>
+    </section>
+  );
+});
